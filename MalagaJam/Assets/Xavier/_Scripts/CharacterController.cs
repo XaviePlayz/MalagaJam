308191d167440bdc635d88d599465e7a126ea05b
Xavier Wevers <xavier.wevers@gmail.com>
2024-01-27 12:17:58 +0100

    Improved NPC System

--- MalagaJam/Assets/Xavier/_Scripts/CharacterController.cs
+++ MalagaJam/Assets/Xavier/_Scripts/CharacterController.cs
@@ -2,12 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UIElements.Experimental;
 
 public class CharacterController : MonoBehaviour
 {
     public GameObject currentNPC;
-    public GameObject[] allAvailableNPCs; 
-    public GameObject canvas;
+    public GameObject destroyPreviousNPC;
+
+    public GameObject[] allAvailableNPCs;
+
+    [Header("Animator")]
+    private Animator anim;
+    private string leavingAnimationTrigger = "Leaving";
 
     [Header("TextBubble")]
     [SerializeField] private GameObject adults;
@@ -17,26 +23,46 @@ public class CharacterController : MonoBehaviour
 
     void Start()
     {
-        CorrectedCurrency();
+        SendNextCharacter();
     }
 
 
     private void SendNextCharacter()
     {
+        currentNPC = allAvailableNPCs[Random.Range(0, allAvailableNPCs.Length)];
+
         Instantiate(currentNPC);
+
+        destroyPreviousNPC = GameObject.FindGameObjectWithTag("NPC");
         adults = GameObject.FindGameObjectWithTag("Adults");
         children = GameObject.FindGameObjectWithTag("Children");
+        anim = GameObject.FindAnyObjectByType<Animator>();
 
         amountOfAdults = Random.Range(1, 4);
         amountOfChildren = Random.Range(1, 4);
 
-        adults.GetComponent<TextMeshProUGUI>().text = "Adults: " + amountOfAdults;
-        children.GetComponent<TextMeshProUGUI>().text = "Children: " + amountOfChildren;
+        if (adults != null && children != null)
+        {
+            adults.GetComponent<TextMeshProUGUI>().text = "Adults: " + amountOfAdults;
+            children.GetComponent<TextMeshProUGUI>().text = "Children: " + amountOfChildren;
+        }
     }
 
     public void CorrectedCurrency()
     {
-        currentNPC = allAvailableNPCs[Random.Range(0, allAvailableNPCs.Length)];
-        SendNextCharacter();
+        StartCoroutine(CustomerLeaves());
+    }
+
+    public IEnumerator CustomerLeaves()
+    {
+        anim.SetTrigger(leavingAnimationTrigger);
+        yield return new WaitForSeconds(3);
+      
+        yield return new WaitForSeconds(1);
+        Destroy(destroyPreviousNPC);
+        destroyPreviousNPC = null;
+
+        yield return new WaitForSeconds(1);
+        SendNextCharacter();     
     }
 }
